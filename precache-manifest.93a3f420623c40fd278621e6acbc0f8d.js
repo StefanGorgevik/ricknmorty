ff99866afad45c6e1cd1623f75b43bc00fbdd107
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-04-29 23:02:45 +0200

    Updates

--- precache-manifest.93a3f420623c40fd278621e6acbc0f8d.js
+++ precache-manifest.93a3f420623c40fd278621e6acbc0f8d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "472ab478d62f6426a848a1ca70abee37",
+    "revision": "41f1a16b03884f92c07a44880000caa1",
     "url": "/ricknmorty/index.html"
   },
   {
-    "revision": "92ec850e7c5dfe32ef02",
-    "url": "/ricknmorty/static/css/main.dca44d87.chunk.css"
+    "revision": "506cc91531676cba44d0",
+    "url": "/ricknmorty/static/css/main.ab01aca2.chunk.css"
   },
   {
     "revision": "e1bcb6c58534b6bd8942",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ricknmorty/static/js/2.322cf153.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "92ec850e7c5dfe32ef02",
-    "url": "/ricknmorty/static/js/main.133ce0c0.chunk.js"
+    "revision": "506cc91531676cba44d0",
+    "url": "/ricknmorty/static/js/main.91f66f07.chunk.js"
   },
   {
     "revision": "ef100d0b05125dbc01fe",
