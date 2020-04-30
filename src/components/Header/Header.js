c04cd0945c947b9085b413198f601ce620b593e6
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-04-30 20:10:35 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,22 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import './Header.css'
 import Form from '../Form/Form'
 import SelectPage from '../SelectPage/SelectPage'
-import {GlobalContext} from '../../context/GlobalState'
-import {ThemeContext} from '../../context/ThemeContext'
+import { GlobalContext } from '../../context/GlobalState'
+import { ThemeContext } from '../../context/ThemeContext'
 
 const Header = () => {
-    const {ui} = useContext(GlobalContext)
-    const {dispatch} = useContext(ThemeContext)
-    
-    return (
-        <nav  style={{background: ui.headerBack, color: ui.color}} className="main-header">
-          <h1>Rick n' morty</h1>  
-          <Form />
-          <SelectPage/>
-          <button style={{border: ui.border}} className="theme-btn" onClick={() => dispatch({type: "SET_THEME"})}>Theme</button>
-        </nav>
-    )
+  const { ui } = useContext(GlobalContext)
+  const { dispatch } = useContext(ThemeContext)
+
+  return (
+    <nav style={{ background: ui.headerBack, color: ui.color }} className="main-header">
+      <h1>Rick n' morty</h1>
+      <Form />
+      <SelectPage />
+      <button style={{ border: ui.border }} className="theme-btn" onClick={() => dispatch({ type: "SET_THEME" })}>Theme</button>
+    </nav>
+  )
 }
 
 export default Header
